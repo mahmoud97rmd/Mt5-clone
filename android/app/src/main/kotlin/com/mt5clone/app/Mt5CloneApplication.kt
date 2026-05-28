a5c1e7464a4aaa2a097b371769f6e73bc2521055
mahmoud97rmd <mahmoud.97.rm@gmail.com>
2026-05-28 06:47:52 +0000

    edition4

--- android/app/src/main/kotlin/com/mt5clone/app/Mt5CloneApplication.kt
+++ android/app/src/main/kotlin/com/mt5clone/app/Mt5CloneApplication.kt
@@ -1,5 +1,6 @@
 package com.mt5clone.app
 
-import io.flutter.app.FlutterApplication
+import io.flutter.embedding.android.FlutterActivity
+import android.app.Application
 
-class Mt5CloneApplication : FlutterApplication()
+class Mt5CloneApplication : Application()
